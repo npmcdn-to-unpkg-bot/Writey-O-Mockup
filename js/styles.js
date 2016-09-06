1aafcd325ce6074fda8755b28b6565085ffed58f
bryanlohjy <bryanlohjy@hotmail.com>
2016-09-06 12:13:36 +1200

    tidied nav

--- js/styles.js
+++ js/styles.js
@@ -1,5 +1,12 @@
+// Setting height of interface section
 $(document).ready(function(){
     var windowHeight = $(window).innerHeight();        //Get the height of the browser window
     var navHeight = $('#navBar').height();
     $('#bodyContainer').height(windowHeight - navHeight);  //Resize the pageContent div, with a size of 60 - page height.
 });
+
+// setting height of nav bar
+// $(document).ready(function(){
+
+// });
+
